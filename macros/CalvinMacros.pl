73c7818c758b587aafea79972c81ed74f1a0f5ea
Randall Pruim <rpuim@gmail.com>
2022-01-04 16:33:00 -0500

    trying RAllEqualChecker() and ggplot()

--- macros/CalvinMacros.pl
+++ macros/CalvinMacros.pl
@@ -11,4 +11,44 @@ sub hash2seed {
   return $seed;
 };
 
+sub ggplot {
+  my (%arg) = (
+        'code' => '',
+        'precode' => '',
+        'theme' => 'theme_bw(base_size = 15)',
+        'width' => 400,
+        'height' => 250,
+        'format' => 'png'
+        @_
+    );
+
+  my $img = rserve_start_plot($arg['format]', $arg['width'], $arg['height']);
+
+  rserve_eval(
+    'library(ggformula); ' .
+    $arg['precode'] . '; ' .
+    'theme_set(' . $arg['theme'] . ');' .
+    'print(' . $arg['code'] . ');'
+  );
+
+  rserve_finish_plot($img);
+}
+
+sub RAllEqualChecker = {
+
+  my (%arg) = (
+        'precode' => '',
+        @_
+    );
+
+  return sub {
+     my ($correct, $student, $ansHash) = @_;  # get correct and student MathObjects
+     my ($student_code) = $student->value;    # get student answer
+     rserve_eval($arg['precode']);
+     my ($score) =
+       rserve_eval("tryCatch(as.integer(isTRUE(all.equal( $correct, $student_code))), error = function(e) FALSE);");
+     return $score;
+   };
+};
+
 1;
